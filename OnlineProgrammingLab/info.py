fb6846d09ee75b767e2207562277333a27e83bdd
204g1a0510 <94513305+204g1a0510@users.noreply.github.com>
2025-02-10 12:36:56 +0530

    Add files via upload

---
+++ OnlineProgrammingLab/info.py
@@ -0,0 +1,6 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'amruthag0202@gmail.com'
+EMAIL_HOST_PASSWORD = 'yels lftr llxk zpsr'  # Use Gmail App Password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
