4e362e8713c78f37cea5bdd0e80e0c058128880e
204g1a0510 <94513305+204g1a0510@users.noreply.github.com>
2025-02-10 12:38:42 +0530

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout, name="logout"),
+    path('activate/<uidb64>/<token>/', views.activate, name="activate"),
+
+    # Password reset views
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
